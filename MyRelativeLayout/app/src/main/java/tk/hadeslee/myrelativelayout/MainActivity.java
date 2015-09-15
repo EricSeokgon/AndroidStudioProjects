7b9f35779a4a18f5336569191dc6872ac418227e
haseslee <hadesleeq@gmail.com>
2015-09-15 15:09:31 +0900

    테이블 레이아웃

--- MyRelativeLayout/app/src/main/java/tk/hadeslee/myrelativelayout/MainActivity.java
+++ MyRelativeLayout/app/src/main/java/tk/hadeslee/myrelativelayout/MainActivity.java
@@ -4,13 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "버튼이 클릭되었습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
