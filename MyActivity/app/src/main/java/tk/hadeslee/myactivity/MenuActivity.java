5c43792aa4e7b9ab17b86dd01a94a7d8a2040836
haseslee <hadesleeq@gmail.com>
2015-09-21 13:23:27 +0900

    메인 액티비티 값을 메뉴 액티비티에 보내서 결과값을 돌려줌

---
+++ MyActivity/app/src/main/java/tk/hadeslee/myactivity/MenuActivity.java
@@ -0,0 +1,53 @@
+package tk.hadeslee.myactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String title = intent.getStringExtra("title");
+            Toast.makeText(getApplicationContext(), "전달 받은 값 : " + title, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void OnButton1Clicked(View v) {
+        Intent intent = new Intent();
+        intent.putExtra("name", "티파니");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
