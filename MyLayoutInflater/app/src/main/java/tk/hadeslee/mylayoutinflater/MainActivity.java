7a85017c7b618d1e075828bcc1386908aa68aec2
haseslee <hadesleeq@gmail.com>
2015-09-21 11:28:03 +0900

    부분화면 추가

--- MyLayoutInflater/app/src/main/java/tk/hadeslee/mylayoutinflater/MainActivity.java
+++ MyLayoutInflater/app/src/main/java/tk/hadeslee/mylayoutinflater/MainActivity.java
@@ -1,9 +1,13 @@
 package tk.hadeslee.mylayoutinflater;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        LinearLayout container = (LinearLayout) findViewById(R.id.container);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.sub_layout, container, true);
     }
 
     @Override
